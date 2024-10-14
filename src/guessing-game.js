1b0f3520f78dcb0d51846f5065f122c9d270cde4
Kirill Tukach <kirill.tukach@gmail.com>
2024-10-14 22:48:04 +0300

    feat: add functions guessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,23 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.currentGuess = null;
+    }
 
     setRange(min, max) {
         this.min = min;
         this.max = max;
-        this.currentGuess = null;
     }
 
     guess() {
-        this.currentGuess = Math.ceil((this.min + this.max) / 2);
+        this.currentGuess = Math.round((this.min + this.max) / 2);
         return this.currentGuess;
     }
 
     lower() {
-        this.max = this.currentGuess - 1;
+        this.max = this.currentGuess;
     }
 
     greater() {
-        this.min = this.currentGuess + 1;
+        this.min = this.currentGuess;
     }
 }
 
